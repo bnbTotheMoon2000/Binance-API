d8384a7676ca6eacd1db330a842e57f272f9f30d
bnbTotheMoon2000 <121224650+bnbTotheMoon2000@users.noreply.github.com>
2023-06-02 10:51:13 +0400

    Create Level2_orderbook.py

---
+++ Level2_orderbook.py
@@ -0,0 +1,93 @@
+# Install the following required packages
+import requests
+import time
+import hashlib
+import hmac
+from urllib.parse import urlencode
+import os 
+main_path = os.path.dirname(os.getcwd())
+api_path = os.path.join(main_path,"PM_package/api_key_storage/spot_real.txt")
+with open(api_path,'r',encoding='utf-8') as f:
+    api_key = f.readline()[:-1]
+    secret_key = f.readline()
+
+class Orderbook:
+    def __init__(self):
+        self.S_URL_V1 = "https://api.binance.com/sapi/v1"
+        self.api_key = api_key
+        self.secret_key = secret_key
+
+    def convert_time(self,ts):
+        from datetime import datetime 
+        time_format = datetime.utcfromtimestamp(ts/1000)
+        return time_format
+
+    def convert_ts(self,tm):
+        import pytz
+        from datetime import datetime
+        t = datetime.strptime(tm,"%Y-%m-%d %H:%M:%S")
+        tz = pytz.timezone('UTC')
+        t_utc = tz.localize(t).astimezone(pytz.utc)
+        ts = int(datetime.timestamp(t_utc))*1000
+        return ts
+
+    # Function to generate the signature
+    def _sign(self,params={}):
+        data = params.copy()
+        ts = str(int(1000 * time.time()))
+        data.update({"timestamp": ts})
+        h = urlencode(data)
+        h = h.replace("%40", "@")
+        b = bytearray()
+        b.extend(self.secret_key.encode())
+        signature = hmac.new(b, msg=h.encode("utf-8"), digestmod=hashlib.sha256).hexdigest()
+        sig = {"signature": signature}
+        return data, sig
+
+
+    # Function to generate the download ID
+    def post(self,path, params={}):
+        sign = self._sign(params)
+        query = urlencode(sign[0]) + "&" + urlencode(sign[1])
+        url = "%s?%s" % (path, query)
+        header = {"X-MBX-APIKEY": self.api_key}
+        resultPostFunction = requests.post(url, headers=header, timeout=30, verify=True)
+        return resultPostFunction
+
+
+    # Function to generate the download link
+    def get(self,path, params):
+        sign = self._sign(params)
+        query = urlencode(sign[0]) + "&" + urlencode(sign[1])
+        url = "%s?%s" % (path, query)
+        header = {"X-MBX-APIKEY": self.api_key}
+        resultGetFunction = requests.get(url, headers=header, timeout=30, verify=True)
+        return resultGetFunction
+
+    def send_request(self,symbol,startTime,endTime,dataType):
+        timestamp = str(int(1000 * time.time()))
+        paramsToObtainDownloadID = {
+            "symbol": symbol,
+            "startTime": self.convert_ts(startTime),
+            "endTime": self.convert_ts(endTime),
+            "dataType": dataType,
+            "timestamp": timestamp,
+        }
+
+        # Calls the "post" function to obtain the download ID for the specified symbol, dataType and time range combination
+        path = "%s/futuresHistDataId" % self.S_URL_V1
+        resultDownloadID = self.post(path, paramsToObtainDownloadID)
+        print(resultDownloadID)
+        try: 
+            downloadID = resultDownloadID.json()["id"]
+        except KeyError as Exception:
+            print(Exception)
+        print(downloadID)  # prints the download ID, example: {'id': 324225}
+
+
+        # Calls the "get" function to obtain the download link for the specified symbol, dataType and time range combination
+        paramsToObtainDownloadLink = {"downloadId": downloadID, "timestamp": timestamp}
+        pathToObtainDownloadLink = "%s/downloadLink" % self.S_URL_V1
+        resultToBeDownloaded = self.get(pathToObtainDownloadLink, paramsToObtainDownloadLink)
+        print(resultToBeDownloaded)
+        print(resultToBeDownloaded.json()) 
