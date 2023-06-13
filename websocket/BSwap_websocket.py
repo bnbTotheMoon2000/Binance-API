375ed3f9cee8c98ac1349b6ce63aac25d450415b
bnbTotheMoon2000 <121224650+bnbTotheMoon2000@users.noreply.github.com>
2023-06-13 20:37:12 +0400

    BSwap_websocket.py

--- websocket/BSwap_websocket.py
+++ websocket/BSwap_websocket.py
@@ -4,29 +4,41 @@ import json
 import hmac
 import hashlib
 import urllib.parse
+import uuid 
+
+def random_string():
+    return str(uuid.uuid1())[:32]
+
+def time_stamp():
+    return str(int(time.time()*1000))
+
+def signature(key,obj):
+    sign = hmac.new(key=key.encode('utf-8'),msg=obj.encode('utf-8'),digestmod=hashlib.sha256)
+    return sign.hexdigest()
+
 
 #websocket.enableTrace(True)
 ws1 = websocket.WebSocket()
 
 api_key = ""
-secret_key = ""
-
+api_secret = ""
+topic = "earn_swapprice_62|earn_swapprice_72"
 base_url = "wss://api.binance.com"
 endpoint_path = "/sapi/wss"
 
-timestamp = round(time.time()*1000)
 params = {
-    "random": "56724ac693184379ae23ffe5e910063c",
-    "topic": "earn_swapprice_62",
+    "random": random_string(),
+    "topic": topic,
     "recvWindow":60000,
-    "timestamp":timestamp
+    "timestamp":time_stamp()
 }
 querystring = urllib.parse.urlencode(params,safe="|")
-
-signature = hmac.new(secret_key.encode('utf-8'),msg = querystring.encode('utf-8'), digestmod = hashlib.sha256).hexdigest()
-url = base_url + endpoint_path + "?" + querystring + "&signature=" + signature.upper()
-
+sign = signature(api_secret,querystring)
+params.update({'signature':sign})
+querystring = urllib.parse.urlencode(params,safe="|")
+url = urllib.parse.urljoin(base_url,endpoint_path)+"?"+querystring
 print(url)
+
 try:
     ws1.connect(url, header={"X-MBX-APIKEY": api_key, "Content-Type": "application/x-www-form-urlencoded"})
     while True:
